56031dc8549e788a0b83448de8f25a700dd1f9c5
Craig Daniels <craigrdaniels@gmail.com>
2025-05-22 06:25:37 +1000

    add access rules to default collections

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,5 +1,8 @@
 import type { CollectionConfig } from 'payload'
 
+import { isAdminField } from '@/access/isAdmin'
+import { isAdminOrSelfFieldLevel } from '@/access/isAdminOrSelf'
+
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
@@ -9,5 +12,18 @@ export const Users: CollectionConfig = {
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      access: {
+        create: isAdminField,
+        read: isAdminOrSelfFieldLevel,
+        update: isAdminField,
+      },
+      name: 'roles',
+      defaultValue: ['user'],
+      options: ['admin', 'user'],
+      hasMany: true,
+      required: true,
+      type: 'select',
+    },
   ],
 }
